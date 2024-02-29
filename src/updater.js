c881de16d8a727c1bad0a4a6ce73d2e2a133b054
Marek Rusinowski <marekrusinowski@gmail.com>
2024-03-01 00:30:21 +0100

    Disable differential downloads

--- src/updater.js
+++ src/updater.js
@@ -6,5 +6,7 @@ autoUpdater.logger = require('electron-log');
 autoUpdater.logger.transports.file.level = 'info';
 
 autoUpdater.autoDownload = false;
+// Installer is small enough, and differential dowload isn't very reliable.
+autoUpdater.disableDifferentialDownload = true;
 
 module.exports = autoUpdater;
